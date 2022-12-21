bb197b63142e005cfcead6b00d6dd30b378ed2f1
JOHAN <johanalesnaaa@gmail>
2022-12-21 12:26:51 +0800

    MILESTONE A - Show the GUI MILESTONE B - Group Radio buttons MILESTONE G - Implement Clearbutton MILESTONE F - Disable text area for list of persons

--- src/App.java
+++ src/App.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class App {
+public class App extends JFrame{
     private JPanel pnlMain;
     private JRadioButton rbCustomer;
     private JRadioButton rbClerk;
@@ -26,10 +28,26 @@ public class App {
     public App() {
         persons = new ArrayList<>();
         // TODO add implementations for all milestones here
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tfAge.setText(null);
+                tfLoad.setText(null);
+                tfMonths.setText(null);
+                tfSalary.setText(null);
+                tfName.setText(null);
+            }
+        });
     }
 
     public static void main(String[] args) {
         // add here how to make GUI visible
+        App app = new App();
+        app.setTitle("");
+        app.setContentPane(app.pnlMain);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.taPersons.setEditable(false);
     }
 
     static void giveReward(int n) {
